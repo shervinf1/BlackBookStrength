47eda2e5845dc6e7464a22cdc2551fd7d576bd0e
shervinf1 <shervinf@outlook.com>
2019-10-14 17:52:32 -0400

    Loginactivity added again

---
+++ app/src/main/java/com/shervinf/blackbookstrength/LoginActivity.java
@@ -0,0 +1,106 @@
+package com.shervinf.blackbookstrength;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // UI references.
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mEmailView = findViewById(R.id.login_email);
+        mPasswordView = findViewById(R.id.login_password);
+
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.integer.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        // TODO: Grab an instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    // Executed when Sign in button pressed
+    public void signInExistingUser(View v)   {
+        // TODO: Call attemptLogin() here
+        attemptLogin();
+
+    }
+
+    // Executed when Register button pressed
+    public void registerNewUser(View v) {
+        Intent intent = new Intent(this, com.shervinf.blackbookstrength.RegisterActivity.class);
+        finish();
+        startActivity(intent);
+    }
+    // TODO: Complete the attemptLogin() method
+    private void attemptLogin() {
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        if (email.equals("") || password.equals(""))
+            return;
+        else
+            Toast.makeText(this, "Login in Progress...", Toast.LENGTH_SHORT).show();
+
+        // TODO: Use FirebaseAuth to sign in with email & password
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("FlashChat", "signInWithEmail() onComplete" + task.isSuccessful());
+
+                if(!task.isSuccessful())
+                {
+                    Log.d("FlashChat", "Problem Logging in: " + task.getException());
+                    showErrorDialog("There was a problem signing in...");
+                }
+                else
+                {
+                    Intent chatIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    finish();
+                    startActivity(chatIntent);
+                }
+
+            }
+        });
+    }
+
+    // TODO: Show error on screen with an alert dialog
+    private void showErrorDialog(String message) {
+        new AlertDialog.Builder(this)
+                .setTitle("Oops")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+}
