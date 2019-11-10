5a7256849a06eb36c63056e11733782766b4d6ab
shervinf1 <shervinf@outlook.com>
2019-11-10 17:07:13 -0400

    changed checkbox implementation

--- app/src/main/java/com/shervinf/blackbookstrength/MainLiftAdapter.java
+++ app/src/main/java/com/shervinf/blackbookstrength/MainLiftAdapter.java
@@ -8,7 +8,7 @@ import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -45,7 +45,7 @@ public class MainLiftAdapter extends RecyclerView.Adapter<MainLiftAdapter.MyView
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView weight, weightUnit, percentage, rep;
-        ImageView checkedImageView;
+        CheckBox checkBoxView;
 
         public MyViewHolder(final View itemView) {
             super(itemView);
@@ -54,17 +54,17 @@ public class MainLiftAdapter extends RecyclerView.Adapter<MainLiftAdapter.MyView
             weightUnit = itemView.findViewById(R.id.weightUnitTextView);
             percentage = itemView.findViewById(R.id.percentageTextView);
             rep = itemView.findViewById(R.id.repTextView);
-            checkedImageView = itemView.findViewById(R.id.checkMarkImageView);
+            checkBoxView = itemView.findViewById(R.id.checkMarkImageView);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     int adapterPosition = getAdapterPosition();
                     if (!itemStateArray.get(adapterPosition, false)) {
-                        checkedImageView.setImageResource(R.drawable.ic_done_blue_24dp);
+                        checkBoxView.setChecked(true);
                         itemStateArray.put(adapterPosition, true);
                     }
                     else  {
-                        checkedImageView.setImageResource(R.drawable.ic_done_white_24dp);
+                        checkBoxView.setChecked(false);
                         itemStateArray.put(adapterPosition, false);
                     }
                 }
@@ -83,7 +83,7 @@ public class MainLiftAdapter extends RecyclerView.Adapter<MainLiftAdapter.MyView
         holder.weightUnit.setText(mainLiftPOJO.getWeightUnit());
         holder.percentage.setText(mainLiftPOJO.getPercentage().toString());
         holder.rep.setText(mainLiftPOJO.getReps());
-//        holder.checkedImageView.setImageResource(mainLiftPOJO.getIsFinished());
+//        holder.checkBoxView.setImageResource(mainLiftPOJO.getIsFinished());
     }
 
 
